e9397e8dec687579875c7e9eb4b63398c9b9a5b7
あ <ritsu.1000000@gmail.com>
2026-09-18 14:53:47 +0900

    Use port 8876 in start_browser.bat

--- start_browser.bat
+++ start_browser.bat
@@ -3,6 +3,10 @@ setlocal
 cd /d "%~dp0"
 title Scratch Comment Bot - Browser UI
 
+set PORT=8876
+set HOST=127.0.0.1
+set OPEN_BROWSER=1
+
 if not exist "scratch-comment-bot-python\start.bat" (
     echo Python browser version was not found.
     echo Run this file from the repository root.
